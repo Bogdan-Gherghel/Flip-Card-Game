0573ff92de17f3c011387461a50e771847311767
Bogdan-Gherghel <34769151+Bogdan-Gherghel@users.noreply.github.com>
2018-05-01 12:11:01 +0100

    initialised variables and added images for cards

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var memory_array = ['a.jpg','a.jpg', 'b.jpg','b.jpg', 'c.jpg', 'c.jpg','d.jpg','d.jpg', 'e.jpg','e.jpg', 'f.jpg','f.jpg', 'g.jpg','g.jpg', 'h.jpg','h.jpg', 'i.jpg','i.jpg', 'j.jpg', 'j.jpg','k.jpg','k.jpg', 'l.jpg','l.jpg'];
+var memory_values = [];
+var memory_tile_ids = [];
+var tiles_flipped = 0;
