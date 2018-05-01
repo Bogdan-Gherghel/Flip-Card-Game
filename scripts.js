63ccee501d5ce55bbb433fd242fdc113323c7a58
Bogdan-Gherghel <34769151+Bogdan-Gherghel@users.noreply.github.com>
2018-05-01 12:17:11 +0100

    added a shuffle method

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@ var memory_array = ['a.jpg','a.jpg', 'b.jpg','b.jpg', 'c.jpg', 'c.jpg','d.jpg','
 var memory_values = [];
 var memory_tile_ids = [];
 var tiles_flipped = 0;
+Array.prototype.memory_tile_shuffle = function () {
+    vari i = this.length, j, temp;
+    while (--i > 0){
+        j - Math.floor(Math.random () * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}
